1b8edaaa5e71944639ddb04bd4ae8bc76f10f7c2
jean <jin.ahn.1027@gmail.com>
2021-05-03 17:24:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+let header = document.createElement("h1");
+header.setAttribute("id", "victory");
+document.body.appendChild(header)
+header.innerHTML = "Jean is the champion";
+
+let newHeader=document.querySelector('h1#victory');
